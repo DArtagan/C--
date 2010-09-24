a35e50e2a78b0243556fcbcdda139a9226f6c43b
William H. Weiskopf <worldwidewilly@comcast.net>
2010-09-24 11:16:53 -0600

    Began code-advice.

---
+++ Work/code-advice/code-advice.cpp
@@ -0,0 +1,54 @@
+//
+//  Program to shift the alphanumeric characters in a file and output them to another file, in the style of a shift-cypher
+//
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    /*  Declare/Identify Variables  */
+    string input;
+    string 
+    char
+    char
+
+    /*  Ask for final value  */
+    cout << "--  --" << endl << endl;
+    cout << "" << flush;
+    cin >> shift; 
+    cout << endl;
+    
+    /*  */
+    ifstream inFile( "ADVICE.TXT" );
+    if( !inFile ) {
+        cout << "Error opening file." << endl;
+        system("PAUSE");
+        exit(1);
+    }
+    ofstream outFile( ADVICE.TXT" );
+    if( !outFile ) {
+        cout << "Error opening file." << endl;
+        system("PAUSE");
+        exit(1);
+    }
+    
+    /*  Shift the characters  */
+    while( ) {
+        getline(inFile, input, '/n')
+    }
+
+    /*  Close Files  */
+    inFile.close();
+    outFile.close();
+    
+    /*  Concluding Text  */
+    cout << endl << endl;
+    cout << "";
+    cout << endl << endl << endl << endl;
+    
+    system("PAUSE");
+    //  Exit Program
+    return 0;
+}
