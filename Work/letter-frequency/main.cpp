84e88f6af792a2d6fa27eb518474748de9d5e0f8
William H. Weiskopf <worldwidewilly@comcast.net>
2010-10-01 11:59:24 -0600

    Began some stuff

--- Work/letter-frequency/main.cpp
+++ Work/letter-frequency/main.cpp
@@ -11,24 +11,37 @@ using namespace std;
 int main()
 {
     /*  Declare/Identify Variables  */
+    int FIRST_LOWERCASE('a');
+    int LAST_LOWERCASE('z');
+    const int alphabet(26);
+    int LetterCounts[alphabet] = {};
+    char letter;
+    int counter(0);
     
 	/*  Open file for reading, check for validity  */
-    ifstream inFile( "" );
+    ifstream inFile( "INPUT.TXT" );
     if( !inFile ) {
         cout << "Error opening file." << endl;
         system("PAUSE");
         exit(1);
     }	
 	
-	/*  Ask for initial values  */
-    cout << "--  --" << endl << endl;
-    cout << "" << flush;
-    cin >> shift; 
+	/*  Header  */
+    cout << "-- Letter Frequency Counter --" << endl << endl;
     cout << endl;
-
+    
+    /*  Store counts in the array  */
+    while(inFile >> letter) {
+        if(letter >= FIRST_LOWERCASE && letter <= LAST_LOWERCASE) {
+            letter >> LetterCounts[counter];
+            counter++;
+        }
+    }
+    
 	/*  Concluding Text  */
-    cout << endl << endl;
-    cout << ;
+	for(int i(0); i<26; i++) {
+        cout << LetterCounts[i];
+    }
     cout << endl << endl << endl << endl;    
     
 	/*  Close Write File  */
