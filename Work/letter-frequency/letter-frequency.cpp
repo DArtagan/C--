739cca3b13b4629021cd8be8fcaee3adf1f4fe7e
William H. Weiskopf <worldwidewilly@comcast.net>
2010-10-02 21:51:48 -0600

    Everything on the letter-frequency assignment except for the second least value and polish.

--- Work/letter-frequency/letter-frequency.cpp
+++ Work/letter-frequency/letter-frequency.cpp
@@ -15,6 +15,8 @@ int main()
     int LAST_LOWERCASE('z');
     const int alphabet(26);
     int LetterCounts[alphabet] = {};
+	string LetterArray[26] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+	int LetterResults[2][4] = {};
     char letter;
     int counter(0);
     
@@ -33,14 +35,37 @@ int main()
     /*  Store counts in the array  */
     while(inFile >> letter) {
         if(letter >= FIRST_LOWERCASE && letter <= LAST_LOWERCASE) {
-            letter >> LetterCounts[counter];
-            counter++;
+			LetterCounts[(letter - 'a')]++;
         }
     }
     
-	/*  Concluding Text  */
+	/*  Find Letters  */
 	for(int i(0); i<26; i++) {
-        cout << LetterCounts[i];
+		if(LetterCounts[i] > LetterResults[0][0]) {
+			LetterResults [0][0] = LetterCounts[i];
+			LetterResults[1][0] = i;
+		}
+		if(LetterCounts[i] > LetterResults[0][1] && LetterCounts[i] < LetterResults[0][0]) {
+			LetterResults [0][1] = LetterCounts[i];
+			LetterResults[1][1] = i;
+		}
+	}
+	LetterResults[0][2] = LetterResults[0][0];
+	LetterResults[0][3] = LetterResults[0][0];
+	for(int i(0); i<26; i++) {
+		if(LetterCounts[i] < LetterResults[0][2]) {
+			LetterResults [0][2] = LetterCounts[i];
+			LetterResults[1][2] = i;
+		}
+		if(LetterCounts[i] < LetterResults[0][3] && LetterCounts[i] > LetterResults[0][2]) {
+			LetterResults [0][3] = LetterCounts[i];
+			LetterResults[1][3] = i;
+		}
+	}
+	
+	/*  Concluding Text  */
+	for(int i(0); i<4; i++) {
+        cout << char(LetterResults[1][i] + FIRST_LOWERCASE) << ": " << LetterResults[0][i] << ", ";
     }
     cout << endl << endl << endl << endl;    
     
