c53c8de120327c3c6dd7009b4d5ff6ae50771006
William H. Weiskopf <worldwidewilly@comcast.net>
2010-10-06 23:06:29 -0600

    Polish almost complete

--- Work/binary-search-words/binary-search-words.cpp
+++ Work/binary-search-words/binary-search-words.cpp
@@ -1,5 +1,5 @@
 //
-//  Program to find a particular word
+//  Program to find a particular word using binary search
 //
 #include <cstdlib>
 #include <iostream>
@@ -10,13 +10,13 @@ using namespace std;
 int main()
 {
     /*  Declare/Identify Variables  */
+	// Constants
 	const int SIZE(50000);
-	string haystack[SIZE] = {};
-	int counter(0);
+	// Variables
+	int counter(0), left, right, middle;
 	string needle;
-	int left;
-	int right;
-	int middle;
+	// Array
+	string haystack[SIZE] = {};
     
 	/*  Open file for reading, check for validity  */
     ifstream inFile( "SORTED.DAT" );
@@ -26,11 +26,11 @@ int main()
         exit(1);
     }	
 	
-	/*  Ask for user chosen word  */
+	/*  Header and prompt the user for a chosen word  */
     cout << "-- Binary Search --" << endl << endl;
 	cout << "What word would you like to find?" << endl;
 	cin >> needle;
-    cout << endl;
+    cout << endl << endl;
 
 	/*  Store data into haystack  */
 	for(int i(0); i < 50000; i++) {
@@ -39,51 +39,44 @@ int main()
 		counter++;
 	}
 	
+	/*  Close read File  */
+    inFile.close();
+	
 	/*  Find left and right values  */
 	left = 0;
 	right = counter - 1;
 	
 	/*  Search  */
-	while( left <= right) {
-		middle = (left + right) / 2;
+	while( left <= right) { // The program keeps going until the left index <=  the right index
+		middle = (left + right) / 2; // Find the middle using an average, because it is an int type, it will truncate
 		if(needle < haystack[middle]) {
-			right = middle - 1;
+			right = middle - 1; // Shrink the search to just the left half
 		} else if(needle == haystack[middle]) {
-			left = middle;
+			left = middle; // Make left and right equal the found value, so the the loop will break
 			right = middle;
 			break;
 		} else {
-			left = middle + 1;
+			left = middle + 1; // Shrink the search to just the right half
 		}
 	}
 	
 	/*  Which side is valid?  */
 	if(left < counter && right > -1) {
-		if(left == right) {
-			cout << "The needle, " << haystack[left] << ", is at point " << left << "." << endl;
-		} else {
-			cout << "The needle is between " << haystack[right] << " and " << haystack[left] << "." << endl;
+		if(left == right) { // When the left equals the right, the word is found.
+			cout << "The word, " << haystack[left] << ", is at point " << left << " in the data set." << endl;
+		} else { // If the left and right sides have crossed, the word is between those two
+			cout << "The word is between " << haystack[right] << " and " << haystack[left] << " in the data set." << endl;
 		}
 	} else {
-		if(!(left < counter)) {
-			cout << "The needle is too big." << endl;
-		} else {
-			cout << "The needle is too small." << endl;
+		if(!(left < counter)) { // If the left bound has gone beyond the data set's upper bound, the word is too large
+			cout << "The word is beyond the upper bound of the data set (too large)." << endl;
+		} else { // If the right bound has gone below the data set's lower bound, the word is too small
+			cout << "The word is below the lower bound of the data set (too small)." << endl;
 		}
 	}
 	
-	/*  Concluding Text  */
-    cout << endl << endl;
-    /*  for(int i(0); i < counter; i++) {
-		cout << haystack[i] << endl;
-	} */
-	cout << left << " , " << right << endl;
-	cout << needle << endl;
-	cout << haystack[left] << " , " << haystack[right];
-    cout << endl << endl << endl << endl;    
-    
-	/*  Close Write File  */
-    inFile.close();
+	/*  Footer  */
+    cout << endl << endl;    
 
 	system("PAUSE");
     //  Exit Program
