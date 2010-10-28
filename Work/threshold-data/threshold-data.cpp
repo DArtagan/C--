8743815f6b7f292ccc5d98582a6dfac99306335f
William H. Weiskopf <worldwidewilly@comcast.net>
2010-10-27 18:20:23 -0600

    Fixed threshold-data.

--- Work/threshold-data/threshold-data.cpp
+++ Work/threshold-data/threshold-data.cpp
@@ -11,15 +11,12 @@ int main()
     /*  Declare/Identify Variables  */
     const int MAX(64);
     int storarray[MAX] = {};
+    int data;
+    int counter(-1);
     int size;
-    int value;
-    int lower;
-    int upper(0);
-    int counter(0);
-    double threshup(.9);
-    double threshdown(.1);
-    double realthreshup;
-    double realthreshdown;
+    int max(0);
+    double lower;
+    double upper;
     
 	/*  Header  */
     cout << "-- Threshhold Filter --" << endl << endl;
@@ -33,50 +30,42 @@ int main()
         exit(1);
     }	
 	
-	/*  Find size bounds and store data  */
-    
-    inFile >> size;
-    while (inFile >> value) {
-        if( !(inFile >> value)) {
-            cout << "You have bad data.";
+	/*  Store data  */
+    while(inFile >> data) {
+        if(counter == -1) {
+            size = data;
+        } else {
+            storarray[counter] = data;
         }
-        if(value < lower) {
-            lower = size;
-        }
-        if(size > upper) {
-            upper = size;
-        }
-        storarray[counter] = size;
         counter++;
     }
     
-    /*  Calculate the thresholds  */
-    realthreshup = ( ( upper - lower ) * threshup + lower ); 
-    realthreshdown = ( ( upper - lower ) * threshdown + lower ); 
-
-    for(int i=0; i <= (counter - 1); i++) {
-        cout << storarray[i];
+    /*  Calculate Max  */
+    for( int i(0); i < size; i++ ) {
+        if( storarray[i] > max ) max = storarray[i];
     }
-
-    /*  Convert Files  */
-    for(int i = 0;i <= counter;i++) {
-        if(storarray[i] < realthreshdown) {
-            storarray[i] = 0;
-        } else if(storarray[i] >= realthreshdown && storarray[i] <= realthreshup) {
-            storarray[i] = 1;
-        } else {
-            storarray[i] = 2;
-        }
-    }    
-
-	/*  Concluding Text  */
-    cout << endl;
-    cout << "Your new matrix looks like: " << endl;
-    for(int i=0; i <= (counter - 1); i++) {
-        cout << storarray[i];
-    }
-    cout << endl << endl << endl << endl;    
     
+    /*  Calculate Thresholds  */
+    upper = max * .9;
+    lower = max * .1;
+    
+    /*  Replace data with indicator constants  */
+    for( int i(0); i < size; i++ ) {
+        if( storarray[i] > upper ) {
+            storarray[i] = 2;
+        } else if( storarray[i] < lower ) {
+            storarray[i] = 0;
+        } else {
+            storarray[i] = 1;
+        }
+    }
+        
+    /*  Concluding Remarks  */
+    for(int i(0); i < size; i++) {
+        cout << storarray[i];
+    }
+    cout << endl << endl << endl;
+
 	/*  Close Write File  */
     inFile.close();
 
